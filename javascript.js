b290f9a5109f09d7ed6c98c212b2093a69c5e7f0
John Maras <sportsballenthusiast@gmail.com>
2022-05-13 17:09:52 -0500

    started to have the input fields do an input on submit

--- javascript.js
+++ javascript.js
@@ -1,5 +1,27 @@
-console.log("Funyons Are Gross");
+$(document).ready(onReady);
+function onReady() {
+  $(document).on("click", "#submitButton", submitInputField);
+}
+function submitInputField() {
+  let firstNameInput = $(`#firstName`).val();
+  console.log("First Name: ", firstNameInput);
+  //tests that this submits to the console
+  let lastNameInput = $(`#lastName`).val();
+  console.log("Last Name: ", lastNameInput);
+  let idInput = $(`#identification`).val();
+  console.log("ID: ", idInput);
+  let titleInput = $(`#title`).val();
+  console.log("Title: ", titleInput);
+  let annualSalaryInput = $(`#annualSalary`).val();
+  console.log("Annual Salary: ", annualSalaryInput);
+  $(`#firstName`).val(``);
+  $(`#lastName`).val(``);
+  $(`#identification`).val(``);
+  $(`#title`).val(``);
+  $(`#annualSalary`).val(``);
+}
 
 //boarder-collapse: collapse; - look into those
 // th, td{
 //   border: 1px, solid; - look into those
+console.log("Funyons Are Gross");
